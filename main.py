5a71006c9cae84823203d1bf6e8680042c2fc7cf
Yekong995 <yekongyt@gmail.com>
2024-02-02 17:12:22 +0800

    Optimize code & remove auth

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from gui import Ui_MainWindow
 from platform import system
 from PyQt5.QtCore import QThread
 from PyQt5.QtGui import QIcon
+from re import compile
 
 user_os = system()
 
@@ -13,11 +14,14 @@ def rename_file(source:str, name:str):
     source_file = source
     ext = source.split(".")[-1]
     new_name = name + "." + ext
-    new_name = source.replace(source.split("\\")[-1], new_name)
+    new_name = source.replace(source.split("\\")[-1], new_name).replace("|", "")
     with open(source_file, "rb") as f:
-        with open(new_name, "wb") as f2:
-            f2.write(f.read())
-            f2.close()
+        try:
+            with open(new_name, "wb") as f2:
+                f2.write(f.read())
+                f2.close()
+        except Exception as e:
+            return
         f.close()
     remove(source_file)
 
@@ -76,22 +80,6 @@ class MainUI(QMainWindow):
             'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
             'merge_output_format': 'mp3'
         }
-        self.ytdl_format_options_login = {
-            'format': 'bestaudio/best',
-            'outtmpl': '%(title)s.%(ext)s',
-            'restrictfilenames': True,
-            'noplaylist': True,
-            'nocheckcertificate': True,
-            'ignoreerrors': False,
-            'logtostderr': False,
-            'quiet': True,
-            'no_warnings': True,
-            'default_search': 'auto',
-            'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-            'merge_output_format': 'mp3',
-            'username': 'username',
-            'password': 'password'
-        }
         if user_os == "Windows":
             from os import environ
             self.path = environ["USERPROFILE"] + '\\Downloads\\' + '%(title)s.%(ext)s'
@@ -107,82 +95,54 @@ class MainUI(QMainWindow):
         self.ui.path.setDisabled(True)
 
         self.ytdl_format_options['outtmpl'] = self.path
-        self.ytdl_format_options_login['outtmpl'] = self.path
 
     def download(self):
         audio = self.ui.audioCheck.isChecked()
         text_ls = self.ui.text.text()
+        url_regex = compile(r'(https?://\S+)')
         if text_ls == "":
-            QMessageBox.critical(self, "Error", "Please input a link or string")
+            QMessageBox.critical(self, "Error", "Please input a link")
+        elif not url_regex.match(text_ls):
+            QMessageBox.critical(self, "Error", "Please input a valid link")
         else:
             if audio:
-                if self.check_login():
-                    self.ytdl_format_options_login['format'] = 'bestaudio/best'
-                    self.ytdl_format_options_login['merge_output_format'] = 'mp3'
-                    self.ytdl_format_options_login['username'] = self.ui.mail.text()
-                    self.ytdl_format_options_login['password'] = self.ui.password.text()
-                else:
-                    self.ytdl_format_options['format'] = 'bestaudio/best'
-                    self.ytdl_format_options['merge_output_format'] = 'mp3'
+                self.ytdl_format_options['format'] = 'bestaudio/best'
+                self.ytdl_format_options['merge_output_format'] = 'mp3'
             else:
-                if self.check_login():
-                    self.ytdl_format_options_login['format'] = 'bestvideo+bestaudio/best'
-                    self.ytdl_format_options_login['merge_output_format'] = 'mp4'
-                    self.ytdl_format_options_login['username'] = self.ui.mail.text()
-                    self.ytdl_format_options_login['password'] = self.ui.password.text()
-                else:
-                    self.ytdl_format_options['format'] = 'bestvideo+bestaudio/best'
-                    self.ytdl_format_options['merge_output_format'] = 'mp4'
-            if self.check_login():
-                self.worker = Background(text_ls, self.ytdl_format_options_login, self.ui)
-            else:
-                self.worker = Background(text_ls, self.ytdl_format_options, self.ui)
+                self.ytdl_format_options['format'] = 'bestvideo+bestaudio/best'
+                self.ytdl_format_options['merge_output_format'] = 'mp4'
+
+            self.worker = Background(text_ls, self.ytdl_format_options, self.ui)
+
             QMessageBox.information(self, "Download", "Download started")
             self.worker.start()
             self.ui.text.setDisabled(True)
             self.ui.audioCheck.setDisabled(True)
             self.ui.downBtn.setDisabled(True)
             self.ui.dsBtn.setDisabled(True)
-            self.ui.mail.setDisabled(True)
-            self.ui.password.setDisabled(True)
             self.worker.finished.connect(self.download_finished)
 
     def search(self):
         audio = self.ui.audioCheck.isChecked()
         text_ls = self.ui.text.text()
         if text_ls == "":
-            QMessageBox.critical(self, "Error", "Please input a link or string")
+            QMessageBox.critical(self, "Error", "Please input a string")
         else:
             if audio:
-                if self.check_login():
-                    self.ytdl_format_options_login['format'] = 'bestaudio/best'
-                    self.ytdl_format_options_login['merge_output_format'] = 'mp3'
-                    self.ytdl_format_options_login['username'] = self.ui.mail.text()
-                    self.ytdl_format_options_login['password'] = self.ui.password.text()
-                else:
-                    self.ytdl_format_options['format'] = 'bestaudio/best'
-                    self.ytdl_format_options['merge_output_format'] = 'mp3'
+                self.ytdl_format_options['format'] = 'bestaudio/best'
+                self.ytdl_format_options['merge_output_format'] = 'mp3'
             else:
-                if self.check_login():
-                    self.ytdl_format_options_login['format'] = 'bestvideo+bestaudio/best'
-                    self.ytdl_format_options_login['merge_output_format'] = 'mp4'
-                    self.ytdl_format_options_login['username'] = self.ui.mail.text()
-                    self.ytdl_format_options_login['password'] = self.ui.password.text()
-                else:
-                    self.ytdl_format_options['format'] = 'bestvideo+bestaudio/best'
-                    self.ytdl_format_options['merge_output_format'] = 'mp4'
-            if self.check_login():
-                self.worker = Background(text_ls, self.ytdl_format_options_login, self.ui, search=True)
-            else:
-                self.worker = Background(text_ls, self.ytdl_format_options, self.ui, search=True)
+                self.ytdl_format_options['format'] = 'bestvideo+bestaudio/best'
+                self.ytdl_format_options['merge_output_format'] = 'mp4'
+
+            self.worker = Background(text_ls, self.ytdl_format_options, self.ui, search=True)
+
             QMessageBox.information(self, "Download", "Download started")
             self.worker.start()
             self.ui.text.setDisabled(True)
             self.ui.audioCheck.setDisabled(True)
             self.ui.downBtn.setDisabled(True)
             self.ui.dsBtn.setDisabled(True)
-            self.ui.mail.setDisabled(True)
-            self.ui.password.setDisabled(True)
             self.worker.finished.connect(self.download_finished)
 
     def download_finished(self):
@@ -192,25 +152,14 @@ class MainUI(QMainWindow):
         self.ui.audioCheck.setDisabled(False)
         self.ui.downBtn.setDisabled(False)
         self.ui.dsBtn.setDisabled(False)
-        self.ui.mail.setDisabled(False)
-        self.ui.password.setDisabled(False)
         self.ui.progressBar.setValue(0)
 
-    def check_login(self):
-        mail = self.ui.mail.text()
-        password = self.ui.password.text()
-        if mail == "" or password == "":
-            return False
-        else:
-            return True
-        
     def change_path(self):
         self.path = QFileDialog.getExistingDirectory(self, "Select Directory", self.path)
         self.path = self.path.replace("/", "\\")
         self.path += '\\%(title)s.%(ext)s'
         self.ui.path.setText(self.path)
         self.ytdl_format_options['outtmpl'] = self.path
-        self.ytdl_format_options_login['outtmpl'] = self.path
         QMessageBox.information(self, "Path", "Path changed to " + self.path)
 
 
